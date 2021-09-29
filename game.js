7b1262e9208588114e7b315fbcf5241300cf5514
CrispyCheeps <lsstonez6@gmail.com>
2021-09-29 10:29:38 +0700

    perubahan ketiga

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-var buttonColours = ["red","blue","green","yellow"];
+var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
 var gameStarted = false;
@@ -7,38 +7,68 @@ var level = 0;
 // dibawah function
 // nextSequence
 
-$(document).keypress(function(){
+$(document).keypress(function() {
   if (!gameStarted) {
     $("#level-title").text("Level " + level);
+    // setTimeout(function(){
+    //   nextSequence()},1000);
     nextSequence();
     gameStarted = true;
   }
 })
 
-$(document).click(function(){
+$("#level-title").click(function() {
   if (!gameStarted) {
     $("#level-title").text("Level " + level);
+    // setTimeout(function(){
+    //   nextSequence()},1000);
     nextSequence();
     gameStarted = true;
   }
 })
 
-$(".btn").click(function(){
+
+$(".btn").click(function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
   animatePress(userChosenColour);
-  checkAnswer(userClickedPattern.length-1);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
+function checkAnswer(currentLevel) {
+  // KALO IF NYA KEBALIK BAKALAN GAGAL
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence()
+      }, 1000);
+    }
 
-function nextSequence(){
 
+  } else {
+
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    startOver();
+  }
+}
+
+function nextSequence() {
+
+  userClickedPattern = [];
   level++;
 
   $("#level-title").text("Level " + level);
 
-  var randomNumber = Math.floor(Math.random()*4);
+  var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
   // using jquery to select the button with the same id as the randomChosenColour
@@ -58,36 +88,14 @@ function animatePress(currentColour) {
 
   $("#" + currentColour).addClass("pressed");
 
-  setTimeout(function(){
+  setTimeout(function() {
     $("#" + currentColour).removeClass("pressed");
-  },100);
+  }, 100);
 }
 
 
-
-function checkAnswer(currentLevel) {
-  // KALO IF NYA KEBALIK BAKALAN GAGAL
-  if (gamePattern[currentLevel]===userClickedPattern[currentLevel]){
-    if(userClickedPattern.length === gamePattern.length){
-      setTimeout(function(){
-        nextSequence()},1000);
-      userClickedPattern.splice(0,userClickedPattern.length);
-    }
-
-
-  }
-  else {
-    $("body").addClass("game-over");
-
-    setTimeout(function(){
-      $("body").removeClass("game-over");
-    },200);
-
-    $("h1").text("Game Over, Press Any Key to Restart");
-    playSound("wrong");
-    gamePattern = [];
-    userClickedPattern = [];
-    gameStarted = false;
-    level = 0;
-  }
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  gameStarted = false;
 }
